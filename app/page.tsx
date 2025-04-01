668273c1f8020e427963ccac2de3edc8cdbba1d9
djiax <djiax03@gmail.com>
2025-04-01 13:42:32 +1030

    spent the entire time finding a font

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen bg-slate-100">
 
-      
+      <div className="py-12 text-center text-black ">
+        A Sacred 
+      </div>
 
 
     </div>
